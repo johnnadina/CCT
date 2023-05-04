8f5cfe0849cb6ec38e5466c00623fdf0d65485b3
johnnadina <joao.diogo.delille@gmail.com>
2023-05-04 17:03:10 +0100

    Update gitpull.lua

--- gitpull.lua
+++ gitpull.lua
@@ -5,7 +5,7 @@ download = http.get(command) --This will make 'download' hold the contents of th
 handle = download.readAll() --Reads everything in download
 download.close() --remember to close download!
 
-rm program --delete the program already in the computer
+shell.run("rm", program)--delete the program already in the computer
 file = fs.open( program,"w") --opens the file with the permissions to write.
 file.write(handle) --writes all the stuff in handle to the file.
 file.close() --remember to close download!
