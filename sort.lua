045b7be203bff59965777152635317d5ce3e6752
shnezzberry <shnezzberry@gmail.com>
2023-05-04 23:36:52 +0200

    Update sort.lua

--- sort.lua
+++ sort.lua
@@ -1,18 +1,18 @@
 
 inbox = {
-    peripheral.wrap("quark:crate_2"),
+    peripheral.wrap("minecraft:barrel_4"),
     peripheral.wrap("quark:variant_chest_9")
 }
 outbox = {}
 storage = {
     ["quark:variant_chest_1"] = {"spruce_log","birch_log","oak_log",""}
+    ["quark:variant_chest_11"] = {"carrot","beetroot","glow_berries","cooked_chicken"}
+    ["quark:variant_chest_12"] = {"bamboo","sugar_cane","glow_berries","cooked_chicken"}
+
 }
---trash = peripheral.wrap("minecraft:chest_")
+trash = peripheral.wrap("quark:variant_chest_10")
 -- make a function here that loads all the switchboard values into the storage box, basically
 
-
-function fetchPattern()
-end
 
 local function isIn (tab, val)
     for index, value in ipairs(tab) do
@@ -32,15 +32,11 @@ end
 
 for i, inv in pairs(inbox) do
     for slot, item in pairs(inv.list()) do
-        print(item.name)
         for chest, pattern in pairs(storage) do
-            for i,v in pairs(pattern) do
-                print(v)
-            end
             if isIn(pattern, rmpfx(item.name)) then
-                inv.pushItems(chest,slot)
-                print("pushing!")
-                print(chest)
+                if inv.pushItems(chest,slot) < 0 then
+                    inv.pushItems(trash.getName,slot)
+                end
             end
         end
     end
